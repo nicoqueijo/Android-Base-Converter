ec95179104de014c6c0d12ff97077466a3e4ce36
nicoqueijo <nicoqueijo@hotmail.com>
2017-03-27 10:00:03 -0400

    Added TODO comments.

--- app/src/main/java/com/nicoqueijo/android/baseconverter/MainActivity.java
+++ app/src/main/java/com/nicoqueijo/android/baseconverter/MainActivity.java
@@ -1,3 +1,24 @@
+// TODO
+// Assure buttons fit in grid in multiple devices
+// TODO
+// Make base labels fade as both seekbars move
+// TODO
+// Disable certain buttons based on fromSeekbar position
+// TODO
+// Reset input if fromSeekbar is moved
+// TODO
+// Add input based on buttons presses
+// TODO
+// Clear input on clear button press
+// TODO
+// Delete last character on delete button press
+// TODO
+// Convert input to output based on toSeekbar
+// TODO
+// Change colour/theme
+// TODO
+// Test on multiple devices and publish to playstore
+
 package com.nicoqueijo.android.baseconverter;
 
 import android.support.v7.app.AppCompatActivity;
