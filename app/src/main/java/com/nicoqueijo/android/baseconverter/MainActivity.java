407982190b0eb8434b3fc5007e330a6dedd266fa
nicoqueijo <nicoqueijo@hotmail.com>
2017-03-28 09:04:18 -0400

    Added more TODOs.

--- app/src/main/java/com/nicoqueijo/android/baseconverter/MainActivity.java
+++ app/src/main/java/com/nicoqueijo/android/baseconverter/MainActivity.java
@@ -17,6 +17,10 @@
 // TODO
 // Change colour/theme
 // TODO
+// Change fonts
+// TODO
+// Find and change app icon/logo
+// TODO
 // Test on multiple devices and publish to playstore
 
 package com.nicoqueijo.android.baseconverter;
