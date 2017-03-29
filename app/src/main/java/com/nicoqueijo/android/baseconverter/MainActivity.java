25cf4a2122393e684a26c298cfc1b3bda10e69fd
nicoqueijo <nicoqueijo@hotmail.com>
2017-03-29 00:01:26 -0400

    Fixed the grid of buttons. Began working on back-end.

--- app/src/main/java/com/nicoqueijo/android/baseconverter/MainActivity.java
+++ app/src/main/java/com/nicoqueijo/android/baseconverter/MainActivity.java
@@ -1,6 +1,4 @@
 // TODO
-// Assure buttons fit in grid in multiple devices
-// TODO
 // Make base labels fade as both seekbars move
 // TODO
 // Disable certain buttons based on fromSeekbar position
@@ -25,14 +23,80 @@
 
 package com.nicoqueijo.android.baseconverter;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Make input seekbar have a starting location of base 10
+    final int SEEKBAR_FROM_START_LOCATION = 8;
+    // Make output seekbar have a starting location of base 2
+    final int SEEKBAR_TO_START_LOCATION = 0;
+
+    private SeekBar seekBarFrom;
+    private SeekBar seekBarTo;
+    private TextView numberLabelFromTwo;
+    private TextView numberLabelFromThree;
+    private TextView numberLabelFromFour;
+    private TextView numberLabelFromFive;
+    private TextView numberLabelFromSix;
+    private TextView numberLabelFromSeven;
+    private TextView numberLabelFromEight;
+    private TextView numberLabelFromNine;
+    private TextView numberLabelFromTen;
+    private TextView numberLabelFromEleven;
+    private TextView numberLabelFromTwelve;
+    private TextView numberLabelFromThirteen;
+    private TextView numberLabelFromFourteen;
+    private TextView numberLabelFromFifteen;
+    private TextView numberLabelFromSixteen;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        seekBarFrom = (SeekBar) findViewById(R.id.seekbar_from_controller);
+        seekBarTo = (SeekBar) findViewById(R.id.seekbar_to_controller);
+        numberLabelFromTwo = (TextView) findViewById(R.id.from_base_label_two);
+        numberLabelFromThree = (TextView) findViewById(R.id.from_base_label_three);
+        numberLabelFromFour = (TextView) findViewById(R.id.from_base_label_four);
+        numberLabelFromFive = (TextView) findViewById(R.id.from_base_label_five);
+        numberLabelFromSix = (TextView) findViewById(R.id.from_base_label_six);
+        numberLabelFromSeven = (TextView) findViewById(R.id.from_base_label_seven);
+        numberLabelFromEight = (TextView) findViewById(R.id.from_base_label_eight);
+        numberLabelFromNine = (TextView) findViewById(R.id.from_base_label_nine);
+        numberLabelFromTen = (TextView) findViewById(R.id.from_base_label_ten);
+        numberLabelFromEleven = (TextView) findViewById(R.id.from_base_label_eleven);
+        numberLabelFromTwelve = (TextView) findViewById(R.id.from_base_label_twelve);
+        numberLabelFromThirteen = (TextView) findViewById(R.id.from_base_label_thirteen);
+        numberLabelFromFourteen = (TextView) findViewById(R.id.from_base_label_fourteen);
+        numberLabelFromFifteen = (TextView) findViewById(R.id.from_base_label_fifteen);
+        numberLabelFromSixteen = (TextView) findViewById(R.id.from_base_label_sixteen);
+
+        seekBarFrom.setProgress(SEEKBAR_FROM_START_LOCATION);
+        seekBarTo.setProgress(SEEKBAR_TO_START_LOCATION);
+
+        seekBarFrom.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
     }
+
 }
